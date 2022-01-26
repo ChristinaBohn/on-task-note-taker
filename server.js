1f5eebd92c0b892737ffbccbb9117208eb5ac699
Christina Bohn <christinajbohn@gmail.com>
2022-01-26 10:55:27 -0800

    Began pseudocode

--- server.js
+++ server.js
@@ -5,10 +5,32 @@ const PORT = 3001;
 
 app.use( express.static('public') );
 
-app.get('/note', (req,res) => {
-    // send the file 'note.html'
-})
+app.get('/notes', (req,res) => {
 
+    // send the file 'notes.html'
+    res.sendFile( path.join(__dirname, 'public/notes.html' ) );
+
+});
+
+app.get('/api/notes', (req,res) => {
+
+    // send the file 'notes.html'
+    res.json( /* send note data */ );
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+    // Create/persist data
+
+    // Access the new note data from 'req'
+
+    // Push it to my existing list of notes
+
+    // Write my updated notes list to the 'db.json' file
+    res.json( 'message' );
+
+});
 
 app.listen(PORT, () =>
     console.log('Exapmle app listening at http://localhost:${PORT}')
